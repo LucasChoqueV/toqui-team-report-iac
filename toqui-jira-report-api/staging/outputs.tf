32cca6d57a116a34462ed45992f16fb4b8915f69
Lucas Choque <lucas@openix.com.ar>
2024-05-08 09:54:56 -0300

    changes

--- toqui-jira-report-api/staging/outputs.tf
+++ toqui-jira-report-api/staging/outputs.tf
@@ -2,3 +2,8 @@ output "trj_instances_kp_private_key_pem" {
   value = module.trj_instances_kp.private_key_pem
   sensitive = true
 }
+
+output "trj_instances_kp_public_key_pem" {
+  value = module.trj_instances_kp.public_key_pem
+  sensitive = true
+}
--- toqui-team-report-web/staging/outputs.tf
+++ toqui-team-report-web/staging/outputs.tf
@@ -2,3 +2,8 @@ output "trw_instances_kp_private_key_pem" {
   value = module.trw_instances_kp.private_key_pem
   sensitive = true
 }
+
+output "trw_instances_kp_public_key_pem" {
+  value = module.trw_instances_kp.public_key_pem
+  sensitive = true
+}
